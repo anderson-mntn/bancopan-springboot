c3c5eb99620aa7dd66c60ebb5f2822a9dffa75a1
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-03-23 14:34:22 -0300

    starting add methods

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,7 +2,15 @@ package com.api.parkingcontrol.services;
 
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
 @Service
 public class ParkingSpotService {
     
+
+final ParkingSpotRepository repository;
+
+public ParkingSpotService(ParkingSpotRepository repository) {
+    this.repository = repository;
+}
 }
